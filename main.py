77ea13c62a9a994be3db39c3e1553cf7aeef28d4
katlegoprince98 <CynicsTech1998@gmail.com>
2023-02-11 06:04:29 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+yourName = input("What is your name? ")
+surname = input("What is your surname? ")
+age = input("What is your age? ")
+favLan = input("What is your favourite programming language? ")
+goal = input("What is your goad? ")
+
+madlibs = f"Hey everyone, my name is {yourName} {surname} and i am {age} years old. My favorite" \
+          f"programming language is {favLan} and my goal is to be a {goal}"
+
+print(madlibs)
